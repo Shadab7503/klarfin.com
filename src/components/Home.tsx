3e8ceac3aafb9423b0a783d7ab779f85da5f1151
amangoyal097 <amangoyal097@gmail.com>
2022-08-21 17:26:14 +0530

    Fix warning errors

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,17 +7,11 @@ import CashManagement from "./CashManagement";
 import Footer from "./Footer";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import data from "../api/data.json";
-import axios from "axios";
-
 const Home = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    // axios(data.item[0].item[0].request)
-    //   .then((response) => console.log(response))
-    //   .catch((err) => console.log(err));
   }, [pathname]);
   return (
     <div className="main">
